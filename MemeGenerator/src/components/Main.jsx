894b12a1035b12a504d7cf57eade0fa1550ad21b
Eleazar Torres <eleazarbtorres03@gmail.com>
2026-02-08 08:36:36 +0800

    Update Main.jsx
    function handleChange(event) {
            const {value, name} = event.currentTarget
            setMeme(prevMeme => ({
                ...prevMeme,
                [name]: value
            }))
        }

--- MemeGenerator/src/components/Main.jsx
+++ MemeGenerator/src/components/Main.jsx
@@ -2,19 +2,23 @@ import React from "react"
 
 export default function Main() {
     const [meme, setMeme] = React.useState({
-    topText: "One does not simply",
+    topText: "Walk into Mordor",
     bottomText: "Walk into Mordor",
     imageUrl: "http://i.imgflip.com/1bij.jpg"
     })
 
     function handleChange(event){
-        const {value} = event.currentTarget
+        const {value, name} = event.currentTarget
         console.log(value)
         setMeme(prevMeme => ({
                                 ...prevMeme, 
-                                bottomText:value}))
+                                [name]:value}))
         
     }
+
+    
+
+    
     return (
         <main>
             <div className="form">
@@ -23,6 +27,8 @@ export default function Main() {
                         type="text"
                         placeholder="One does not simply"
                         name="topText"
+                        onChange={handleChange}
+                        value={meme.topText}
                        
                     />
                 </label>
@@ -33,6 +39,7 @@ export default function Main() {
                         placeholder="Walk into Mordor"
                         name="bottomText"
                         onChange={handleChange}
+                        value={meme.bottomText}
                         
                     />
                 </label>
