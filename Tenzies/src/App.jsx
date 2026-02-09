b4ad0a1081df4bb194064b878187d1387caa4764
Eleazar Torres <eleazarbtorres03@gmail.com>
2026-02-09 20:27:13 +0800

    easy random number generator
         return new Array(10)
                .fill(0)
                .map(() => Math.ceil(Math.random() * 6))

--- Tenzies/src/App.jsx
+++ Tenzies/src/App.jsx
@@ -1,19 +1,13 @@
 
 import Main from "./components/Main"
 
-function allNewDice(){
-     const randomArr = [];
-        for (let i = 0; i < 10; i++) {
-            
-            const randomNumber = Math.floor(Math.random() * 6)+1;
-            randomArr.push(randomNumber);
-        }
-         console.log(randomArr)
-        return randomArr;
+    function allNewDice() {
+        return new Array(10)
+            .fill(0)
+            .map(() => Math.ceil(Math.random() * 6))
+    }
        
-}
 
-allNewDice()
 
 
 export default function App(){
