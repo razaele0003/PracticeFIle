1c0d6b0866c1a764c47673795d1fef0e5baa9449
Eleazar Torres <eleazarbtorres03@gmail.com>
2026-02-24 01:19:03 +0800

    Random Number Reload
    /**
     * Challenge:
     *
     * Create state to hold our array of numbers. (Initialize
     * the state by calling our `generateAllNewDice` function so it
     * loads all new dice as soon as the app loads)
     *
     * Map over the state numbers array to generate our array
     * of Die elements and render those in place of our
     * manually-written 10 Die elements.
     */

--- Tenzies/src/App.jsx
+++ Tenzies/src/App.jsx
@@ -1,15 +1,25 @@
+import Die from "./components/Die"
+import React from "react"
 
-import Main from "./components/Main"
 
-    function allNewDice() {
+export default function App() {
+    const [number, setNumber] = React.useState(generateAllNewDice())
+    
+    function generateAllNewDice() {
         return new Array(10)
             .fill(0)
             .map(() => Math.ceil(Math.random() * 6))
     }
-       
-
-
-
-export default function App(){
-    return(<Main/>)
+    
+     const diceElements = number.map((num, index) => (
+        <Die key={index} value={num} />
+    ))
+    
+    return (
+        <main>
+            <div className="dice-container">
+               {diceElements}
+            </div>
+        </main>
+    )
 }
