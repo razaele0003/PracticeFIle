b4896303e0e2883542e89692eab8c35503533861
Eleazar Torres <eleazarbtorres03@gmail.com>
2026-04-26 07:21:30 +0800

    Add type to orderId
    /**
     * Challenge: Teach TS what data type should be used for the
     * orderId in the completeOrder function. Then check for any
     * additional warnings TS comes up with and fix those.
     */

---
+++ Typescript/pizzaApp/index.ts
@@ -0,0 +1,54 @@
+const menu = [
+    { name: "Margherita", price: 8 },
+    { name: "Pepperoni", price: 10 },
+    { name: "Hawaiian", price: 10 },
+    { name: "Veggie", price: 9 },
+]
+
+let cashInRegister: number = 100
+let nextOrderId: number = 1
+let orderQueue: Order[] = []
+
+type Pizza = {
+    name: string
+    price: number
+}
+
+type Order = {
+    id: number
+    pizza: Pizza
+    status: string
+}
+
+
+function addNewPizza(name, price) {
+    menu.push({ name, price })
+}
+
+function placeOrder(pizzaName) {
+    const selectedPizza = menu.find(pizzaObj => pizzaObj.name === pizzaName)
+    cashInRegister += selectedPizza.price
+    const newOrder = { id: nextOrderId++, pizza: selectedPizza, status: "ordered" }
+    orderQueue.push(newOrder)
+    return newOrder
+}
+
+function completeOrder(orderId: number) {
+    const order = orderQueue.find(order => order.id === orderId)
+    if (!order) {
+        console.log("no order")
+        return ``
+    }
+
+    order.status = "completed"
+    return order
+}
+
+
+addNewPizza("Adobo Pizza", 12)
+
+console.log("menu : ", menu)
+console.log(placeOrder("Adobo Pizza"))
+
+console.log(orderQueue)
+console.log(cashInRegister)     
